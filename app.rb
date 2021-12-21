862960a2451ab3e4b6f2a35d0c0f8eed69bc1f47
Fiona Bompard <fiona@toucanbox.com>
2021-12-21 15:58:53 +0100

    Set up Sinatra and Capistrano

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'json'
+require 'digest'
+
+class App < Sinatra::Base
+  before do
+    content_type :json
+  end
+
+  get '/' do
+    { status: :ok }.to_json
+  end
+
+  get '/:name' do
+    {
+      name: params[:name],
+      digest: Digest::SHA256.base64digest(params[:name])
+    }.to_json
+  end
+end
